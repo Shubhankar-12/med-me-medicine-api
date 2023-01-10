0e159d46c94dfb1b54d5cb406b43a8ad8521e850
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-10 22:27:21 +0530

    index

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
+require("dotenv").config();
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 const app = express();
+const routes = require("./routes/route");
+const dbLink = process.env.URI;
 
-const uri = `mongodb+srv://Shubh:${process.env.password}@cluster0.90qsz.mongodb.net/medMeMedicinesDB`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect((err) => {
-  const collection = client.db("medMeMedicinesDB").collection("medicines");
-  app.get("/medicines", (req, res) => {
-    collection.find({}).toArray((err, documents) => {
-      res.send(
-        documents.map((doc) => ({
-          "web-scraper-order": doc["web-scraper-order"],
-          "web-scraper-start-url": doc["web-scraper-start-url"],
-          link_to_med: doc["link_to_med"],
-          "link_to_med-href": doc["link_to_med-href"],
-          med_name: doc["med_name"],
-          med_manufacturer: doc["med_manufacturer"],
-          med_salt: doc["med_salt"],
-          med_uses: doc["med_uses"],
-          med_price: doc["med_price"],
-        }))
-      );
-    });
-  });
-  app.listen(3000, () => console.log("API listening on port 3000"));
+mongoose.connect(dbLink);
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.log(error);
 });
+
+db.once("connected", () => {
+  console.log("Database Connected");
+});
+
+app.use(express.json());
+app.use("/api", routes);
+
+app.listen(3000, () => console.log("API listening on port 3000"));
